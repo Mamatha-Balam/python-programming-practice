fb6637dee701281d3c003ab967210248d56000aa
Mamatha-Balam <mamatha.balam26@gmail.com>
2026-04-02 00:47:08 +0530

    Added Day1

---
+++ python-programming-practice-main/DAY1/day1.py
@@ -0,0 +1,30 @@
+# 1. Print your name
+print("Mamatha Balam")
+
+# 2. Addition
+print(10 + 5)
+
+# 3. Subtraction
+print(10 - 5)
+
+# 4. Multiplication
+print(10 * 5)
+
+# 5. Division
+print(10 / 5)
+
+
+# Variables example
+a = "Mamatha"
+b = "is learning"
+c = "Python"
+print(a + " " + b + " " + c)
+
+# Number variables
+x = 7
+y = 3
+print(x + y)
+
+# Taking input from user
+name = input("Enter your name: ")
+print("Your name is " + name)
