59eb4aaecdfd8d24bdcf30877628b7547f7af53d
Mamatha-Balam <mamatha.balam26@gmail.com>
2026-04-02 22:29:48 +0530

    day2

---
+++ DAY2/day2.py
@@ -0,0 +1,38 @@
+# Variables example
+a = "Maya"
+print(a)
+
+# Updating variable
+a = "Learning Python"
+print(a)
+
+
+# Integer data type
+my_integer = 42
+print(my_integer)
+
+# Float data type
+my_float = 3.14
+print(my_float)
+
+# String data type
+my_string = "Hello, World!"
+print(my_string)
+
+# Boolean data type
+is_true = True
+is_false = False
+print(is_true)
+print(is_false)
+
+
+# Mixed example
+name = "Maya"
+age = 21
+height = 5.3
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
